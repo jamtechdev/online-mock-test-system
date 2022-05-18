5fb1c7080ba7511b6f1e3f5dac2e4e12859ee76c
jamtechba <jamtechba@gmail.com>
2022-05-18 19:29:03 +0530

    auth

--- resources/views/candidate.blade.php
+++ resources/views/candidate.blade.php
@@ -1,48 +1,35 @@
-@extends('appdata')
+@extends('layouts.app')
 
 @section('content')
 
 <div class="container  my-3">
-    <form action="" method="post">
         <div class="row">
-            <div class="col sm-4"></div>
-            <div class="col-sm-4">
+    <form action="" method="post" class="border border-dark">
+        <div class="row">
+            <div class="col-sm-6">
                 <label for="name">Conditate Name</label>
                 <input type="text" name="name" class="form-control" placeholder="Enter Candidate Name">
             </div>
-            <div class="col-sm-4"></div>
-        </div>
-
-        <div class="row">
-            <div class="col sm-4"></div>
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <label for="email">Conditate Email</label>
                 <input type="text" name="email" class="form-control" placeholder="Enter Candidate Email">
             </div>
-            <div class="col-sm-4"></div>
+
         </div>
 
         <div class="row">
-            <div class="col sm-4"></div>
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <label for="phone">Conditate phone</label>
                 <input type="text" name="phone" class="form-control" placeholder="Enter Candidate Contact">
             </div>
-            <div class="col-sm-4"></div>
-        </div>
-
-        <div class="row">
-            <div class="col sm-4"></div>
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <label for="address">Conditate Address</label>
                <textarea name="address" id="" class="form-control" placeholder="Enter Conditate Addesss"></textarea>
             </div>
-            <div class="col-sm-4"></div>
         </div>
 
         <div class="row">
-            <div class="col sm-4"></div>
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                 <label for="skill">Select Skills</label>
                 <br>
               <select name="skill" id="" class="form-control">
@@ -53,14 +40,17 @@
                   <option value="android">Android</option>
               </select>
             </div>
-            <div class="col-sm-4"></div>
-        </div>
+            <div class="col-sm-6">
+                <label for="experience">Condidate Experience</label>
+                <input type="text" name="experience"class="form-control" placeholder="Enter Candidate Contact" >
+            </div>
 
+        </div>
         <div class="row">
             <div class="col sm-4"></div>
             <div class="col-sm-4">
-                <label for="experience">Condidate Experience</label>
-                <input type="text" name="experience"class="form-control" placeholder="Enter Candidate Contact" >
+                <br>
+                <input type="submit" name="submit"class="form-control" >
             </div>
             <div class="col-sm-4"></div>
         </div>
